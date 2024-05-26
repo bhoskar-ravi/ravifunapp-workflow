683e57366c0fafdeda36050776d2976e5c0e00d1
bhoskar-ravi <ravibhoskar@outlook.com>
2024-05-26 12:06:03 +0530

    commit

--- ravifunapp-workflow/Function1.cs
+++ ravifunapp-workflow/Function1.cs
@@ -27,7 +27,7 @@ namespace ravifunapp_workflow
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. Auto Deployed from function. This HTTP triggered function executed successfully.";
+                : $"Hello, {name}. Automatic. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
         }
